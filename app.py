ed562f535ad73cf5c42d304a935f47af1263fa86
Projectproakash26 <akash@projectpro.io>
2024-07-10 10:48:56 +0530

    Update region in app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = FastAPI()
 def fetch_data(year: int = None, country: str = None, market: str = None):
     try:
         # Load CSV content into a pandas DataFrame
-        df = pd.read_csv("https://my-bucket-akash.s3.eu-north-1.amazonaws.com/total_data.csv")
+        df = pd.read_csv("https://akash-bucket26.s3.ap-south-1.amazonaws.com/total_data.csv")
 
         print(df.shape[0])
         # Apply filters based on provided parameters
